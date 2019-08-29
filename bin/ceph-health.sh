13d3df428ab2ba53b70b061fdc8385dadbecaea4
Thorsten Müller <t.mueller@denic.de>
2019-08-29 09:39:34 +0200

    new version

--- bin/ceph-health.sh
+++ bin/ceph-health.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 ceph=$(which ceph)
-$ceph health
-rc=$?
-exit $?
+status=$($ceph health)
+if [[ $stats =~ HEALTH_OK ]]; then
+  echo $status
+	exit 0
+else
+  echo $status
+	exit 2
+fi
--- bin/metrics-ceph.py
+++ bin/metrics-ceph.py
@@ -17,7 +17,7 @@ def main():
 	print("ceph.{:s}.bytes_used {:d} {:d}".format(args.env, l['pgmap']['bytes_used'], ts))
 	print("ceph.{:s}.bytes_avail {:d} {:d}".format(args.env, l['pgmap']['bytes_avail'], ts))
 	print("ceph.{:s}.bytes_total {:d} {:d}".format(args.env, l['pgmap']['bytes_total'], ts))
-	print("ceph.{:s}.read_bytes_sec {:d} {:d}".format(args.env, l['pgmap']['read_bytes_sec'], ts))
+	#print("ceph.{:s}.read_bytes_sec {:d} {:d}".format(args.env, l['pgmap']['read_bytes_sec'], ts))
 	print("ceph.{:s}.write_bytes_sec {:d} {:d}".format(args.env, l['pgmap']['write_bytes_sec'], ts))
 	print("ceph.{:s}.read_op_per_sec {:d} {:d}".format(args.env, l['pgmap']['read_op_per_sec'], ts))
 	print("ceph.{:s}.write_op_per_sec {:d} {:d}".format(args.env, l['pgmap']['write_op_per_sec'], ts))
